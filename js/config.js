40f10b3819c57665217a2d4d7453afb544d64ce5
andrew_magri <andrewmagri@alphafx.co.uk>
2022-07-25 10:23:27 +0200

    Updated config.js file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_pdpI7PpnY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3km25jrfccnti1mtgegm0va0rk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
